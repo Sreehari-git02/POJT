c1dabfeedb699e63554534622ff52e397558e643
Sreehari <sreeharipaya@gmail.com>
2025-12-25 16:04:34 +0530

    second commit

--- code/index.php
+++ code/index.php
@@ -4,8 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="../style/home.css">
 </head>
 <body>
-    
+    <nav class="navBar">
+        <div class="navBrand">OJT</div>
+        <ul class="navLinks">
+            <li><a href="index.php" class="activeLink">Home</a></li>
+            <li><a href="#" class="logoutLink">Logout</a></li>
+        </ul>
+    </nav>
+
+    <main class="mainContent">
+        <div class="welcomeCard">
+            <h1 class="welcomeTitle">Welcome Back!</h1>
+            <p class="welcomeText">We're glad to see you again. Explore your dashboard and manage your account easily.</p>
+            <div class="actionButtons">
+                <button class="primaryBtn">View Profile</button>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
